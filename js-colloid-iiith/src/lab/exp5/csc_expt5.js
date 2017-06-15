536ea7b56bbdbad127b7ecb82b2c50b9b89c87bc
MaheeSurya <16101998ms@gmail.com>
2017-06-16 05:01:02 +0530

    Experiment-5 50%

--- js-colloid-iiith/src/lab/exp5/csc_expt5.js
+++ js-colloid-iiith/src/lab/exp5/csc_expt5.js
@@ -96,6 +96,8 @@ var view={
 		this.addClickEvent('flsk',function(){ view.water();});
 		this.addClickEvent('salt',function(){ view.callSalt();});
 		this.addClickEvent('sgr',function(){ view.callSugar();});
+		this.addClickEvent('dtr',function(){ view.callDeter();});
+		this.addClickEvent('feoh',function(){ view.callFeoh();});
 	},
 
 	water: function(){
@@ -314,58 +316,292 @@ var view={
 					}
 				},50)
 			},2800);
-		// 	setTimeout(function(){
-		// 		$("#beak2").css("top","-2%");
-		// 		$("#beak2").css("left","22.7%");
-		// 		this.z=205;
-		// 		time5=setInterval(function(){
-		// 			water=[];
-		// 			for(k=205;k<=294;k++)
-		// 			{
-		// 				water[k]="images/sprites/sugar_salt/" + k + ".png";
-		// 			}
-		// 			if(this.z<water.length){
-		// 				$("#beak2").attr("src",water[this.z]);
-		// 			}
-		// 			if(this.z==213)
-		// 			{
-		// 				$("#ar3_txt").show();
-		// 			}
-		// 			if(this.z == 265){
-		// 				$("#ar3_txt").hide();
-		// 			}
-		// 			this.z++;
-		// 			if(this.z == 295){
-		// 			 	$("#rod1").show();
-		// 				clearInterval(time5);
-		// 			}
-		// 		},50)
-		// 	},6000)
-		// 	 setTimeout(function(){
-		// 	 	this.x=0;
-		// 	 	time6=setInterval(function(){
-		// 	 		for(j=0;j<=59;j++)
-		// 			{
-		// 				water[j]="images/sprites/stirring/" + j + ".png";
-		// 			}
-		// 			if(this.x<water.length){
-		// 				$("#rod1").attr("src",water[this.x]);
-		// 			}
-		// 			this.x++;
-		// 			if(this.x == 60){
-		// 				$("#rod1").hide();
-		// 				clearInterval(time6);
-		// 				$("#tag2").show();
-		// 				$("#salt2_txt").show();
-		// 				setTimeout(function(){
-		// 					view.animateDirection_1('tag2', '-=3', '-=0', '1000');
-		// 				},500);
-		// 				setTimeout(function(){
-		// 					view.animateDirection_1('salt2_txt', '-=3', '-=0', '1000');
-		// 				},500);
-		// 			}
-		// 		},50)
-		// 	 },10000)
+			setTimeout(function(){
+				$("#beak3").css("top","-2%");
+				$("#beak3").css("left","33.8%");
+				this.z=205;
+				time9=setInterval(function(){
+					water=[];
+					for(k=205;k<=294;k++)
+					{
+						water[k]="images/sprites/sugar_salt/" + k + ".png";
+					}
+					if(this.z<water.length){
+						$("#beak3").attr("src",water[this.z]);
+					}
+					if(this.z==213)
+					{
+						$("#ar5_txt").show();
+					}
+					if(this.z == 265){
+						$("#ar5_txt").hide();
+					}
+					this.z++;
+					if(this.z == 295){
+					 	$("#rod2").show();
+						clearInterval(time9);
+					}
+				},50)
+			},6000)
+			setTimeout(function(){
+			 	this.x=0;
+			 	time10=setInterval(function(){
+			 		for(j=0;j<=59;j++)
+					{
+						water[j]="images/sprites/stirring/" + j + ".png";
+					}
+					if(this.x<water.length){
+						$("#rod2").attr("src",water[this.x]);
+					}
+					this.x++;
+					if(this.x == 60){
+						$("#rod2").hide();
+						clearInterval(time10);
+						$("#tag3").show();
+						$("#sugar_txt").show();
+						setTimeout(function(){
+							view.animateDirection_1('tag3', '-=3', '-=0', '1000');
+						},500);
+						setTimeout(function(){
+							view.animateDirection_1('sugar_txt', '-=3', '-=0', '1000');
+						},500);
+					}
+				},50)
+			 },10000)
+		}
+	},
+
+	callDeter: function(){
+		if(this.step_no == 3)
+		{
+			$("#back4").show();
+			$("#beak4").show();
+			this.animateDirection_1('beak4', '+=38', '-=40', '1000');
+			this.step_no++;
+			setTimeout(function(){
+			$("#flask4").show();
+			$("#arrow5").show();
+			$("#ar6_txt").show();
+			},1000);
+			setTimeout(function(){
+				this.x=1;
+				$("#arrow5").hide();
+				$("#ar6_txt").hide();
+				time11=setInterval(function(){
+					images=[];
+					for(i=1;i<=69;i++)
+					{
+						images[i]= "images/sprites/water_flasks/"  + i + ".png";
+					}
+					if(this.x<images.length)
+					{
+						$("#flask4").attr("src",images[this.x]);
+					}
+					this.x++;
+					if(this.x ==70){
+						$("#flask4").hide();
+						clearInterval(time11);
+					}
+				},50);
+			},3000);
+			setTimeout(function(){
+				this.y=1;
+				time12=setInterval(function(){
+					water=[];
+					for(j=0;j<=48;j++)
+					{
+						water[j]="images/sprites/water_beak/" + j + ".png";
+					}
+					if(this.y<water.length){
+						$("#beak4_water").attr("src",water[this.y]);
+					}
+					this.y++;
+					if(this.y == 49){
+						clearInterval(time12);
+					}
+				},50)
+			},2800);
+			setTimeout(function(){
+				$("#beak4").css("top","-2%");
+				$("#beak4").css("left","45.8%");
+				this.z=205;
+				time13=setInterval(function(){
+					water=[];
+					for(k=205;k<=294;k++)
+					{
+						water[k]="images/sprites/detergent_mixing/" + k + ".png";
+					}
+					if(this.z<water.length){
+						$("#beak4").attr("src",water[this.z]);
+					}
+					if(this.z==213)
+					{
+						$("#ar7_txt").show();
+					}
+					if(this.z == 265){
+						$("#ar7_txt").hide();
+					}
+					this.z++;
+					if(this.z == 295){
+					 	$("#rod3").show();
+						clearInterval(time13);
+					}
+				},50)
+			},6000)
+			setTimeout(function(){
+			 	this.x=0;
+			 	this.a=100;
+			 	this.b=0;
+			 	time14=setInterval(function(){
+			 		for(j=0;j<=59;j++)
+					{
+						water[j]="images/sprites/stirring/" + j + ".png";
+					}
+					if(this.x<water.length){
+						$("#rod3").attr("src",water[this.x]);
+						this.a-=1.8;
+						if(this.a<0)
+							this.a=0;
+						this.b=this.a +"%"; 
+						$("#beak4_water").css("-webkit-filter","grayscale("+this.b+")");
+					}
+					this.x++;
+					if(this.x == 55){
+						$("#beak4_water").css("top","58.6%");
+						$("#beak4_water").attr("src","images/shapes/457.png");
+					}
+					if(this.x == 70)
+					{
+						$("#rod3").hide();
+						clearInterval(time14);
+						$("#tag4").show();
+						$("#deter_txt").show();
+						setTimeout(function(){
+							view.animateDirection_1('tag4', '-=3', '-=0', '1000');
+						},500);
+						setTimeout(function(){
+							view.animateDirection_1('deter_txt', '-=3', '-=0', '1000');
+						},500);
+					}
+				},50)
+			 },10000)
+		}
+	},
+
+	callFeoh: function(){
+		if(this.step_no == 4)
+		{
+			$("#back5").show();
+			$("#beak5").show();
+			this.animateDirection_1('beak5', '+=38', '-=28', '1000');
+			this.step_no++;
+			setTimeout(function(){
+			$("#flask5").show();
+			$("#arrow6").show();
+			$("#ar8_txt").show();
+			},1000);
+			setTimeout(function(){
+				this.x=1;
+				$("#arrow6").hide();
+				$("#ar8_txt").hide();
+				time15=setInterval(function(){
+					images=[];
+					for(i=1;i<=69;i++)
+					{
+						images[i]= "images/sprites/feoh_flasks/"  + i + ".png";
+					}
+					if(this.x<images.length)
+					{
+						$("#flask5").attr("src",images[this.x]);
+					}
+					this.x++;
+					if(this.x ==70){
+						$("#flask5").hide();
+						clearInterval(time15);
+					}
+				},50);
+			},3000);
+			setTimeout(function(){
+				this.y=1;
+				time12=setInterval(function(){
+					water=[];
+					for(j=0;j<=48;j++)
+					{
+						water[j]="images/sprites/water_beak/" + j + ".png";
+					}
+					if(this.y<water.length){
+						$("#beak5_water").attr("src",water[this.y]);
+					}
+					this.y++;
+					if(this.y == 49){
+						clearInterval(time12);
+					}
+				},50)
+			},2800);
+			// setTimeout(function(){
+			// 	$("#beak4").css("top","-2%");
+			// 	$("#beak4").css("left","45.8%");
+			// 	this.z=205;
+			// 	time13=setInterval(function(){
+			// 		water=[];
+			// 		for(k=205;k<=294;k++)
+			// 		{
+			// 			water[k]="images/sprites/detergent_mixing/" + k + ".png";
+			// 		}
+			// 		if(this.z<water.length){
+			// 			$("#beak4").attr("src",water[this.z]);
+			// 		}
+			// 		if(this.z==213)
+			// 		{
+			// 			$("#ar7_txt").show();
+			// 		}
+			// 		if(this.z == 265){
+			// 			$("#ar7_txt").hide();
+			// 		}
+			// 		this.z++;
+			// 		if(this.z == 295){
+			// 		 	$("#rod3").show();
+			// 			clearInterval(time13);
+			// 		}
+			// 	},50)
+			// },6000)
+			// setTimeout(function(){
+			//  	this.x=0;
+			//  	this.a=100;
+			//  	this.b=0;
+			//  	time14=setInterval(function(){
+			//  		for(j=0;j<=59;j++)
+			// 		{
+			// 			water[j]="images/sprites/stirring/" + j + ".png";
+			// 		}
+			// 		if(this.x<water.length){
+			// 			$("#rod3").attr("src",water[this.x]);
+			// 			this.a-=1.8;
+			// 			if(this.a<0)
+			// 				this.a=0;
+			// 			this.b=this.a +"%"; 
+			// 			$("#beak4_water").css("-webkit-filter","grayscale("+this.b+")");
+			// 		}
+			// 		this.x++;
+			// 		if(this.x == 55){
+			// 			$("#beak4_water").css("top","58.6%");
+			// 			$("#beak4_water").attr("src","images/shapes/457.png");
+			// 		}
+			// 		if(this.x == 70)
+			// 		{
+			// 			$("#rod3").hide();
+			// 			clearInterval(time14);
+			// 			$("#tag4").show();
+			// 			$("#deter_txt").show();
+			// 			setTimeout(function(){
+			// 				view.animateDirection_1('tag4', '-=3', '-=0', '1000');
+			// 			},500);
+			// 			setTimeout(function(){
+			// 				view.animateDirection_1('deter_txt', '-=3', '-=0', '1000');
+			// 			},500);
+			// 		}
+			// 	},50)
+			//  },10000)
 		}
 	},
 };
