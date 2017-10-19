27c607ed7d1ad135ed29fc25b841f4011335303c
Mahee_Surya <16101998ms@gmail.com>
2017-10-19 14:47:17 +0530

    part 1 completed

--- js-colloid-iiith/lab/exp3/csc_exp3.js
+++ js-colloid-iiith/lab/exp3/csc_exp3.js
@@ -1,7 +1,23 @@
+window.onload = function(){ 
+	addPointer("#struc");
+	addPointer("#action");
+	addPointer("#form");
+	addPointer("#close_btn1");
+    addclickEvents();
+}
+
+// addPointer: Adds a pointer to the object to be clicked
+function addPointer(Id){
+	$(Id).css("cursor","pointer");
+}
+
 //This function adds click events to all clickable objects
 function addclickEvents(){
-    document.getElementById("action").addEventListener("click", function() {
+    document.getElementById("struc").addEventListener("click", function() {
         callAction();
+    }, false);
+    document.getElementById("close_btn1").addEventListener("click", function() {
+        callClose1();
     }, false);
 }
 
@@ -12,5 +28,28 @@ function move(id,left,top,time){
 function callAction() {
 		$("#buttons").hide();
 		$("#div_btn1").show();
+		setTimeout(function() {
+			$("#head").on("mouseout",function(){
+				$("#head_red").velocity({opacity: 0},{duration: 500});
+				$("#infoh").hide();
+			})
+			$("#head").on("mouseover",function(){
+				$("#head_red").velocity({opacity: 0.9},{duration: 1000});
+				$("#infoh").show();
+			})
+			$("#tail").on("mouseout",function(){
+				$("#tail_red").velocity({opacity: 0},{duration: 500});
+				$("#infotl").hide();
+			})
+			$("#tail").on("mouseover",function(){
+				$("#tail_red").velocity({opacity: 0.9},{duration: 1000});
+				$("#infotl").show();
+			})
+		}, 1500);
+		move("#infobg",0,134,1500);
 }
 
+function callClose1(){
+	$("#div_btn1").hide();
+	$("#buttons").show();
+}
